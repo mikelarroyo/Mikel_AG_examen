84a20d7d1a788b976ca3574ddc284500fb7ab129
Mikel <mikelarroyogomez@gmail.com>
2025-12-10 14:00:59 +0100

    Actividad 8

--- examen/miligram/js/acordeon.js
+++
@@ -1,6 +0,0 @@
-document.querySelectorAll('.acordeon-titulo').forEach(btn => {
-  btn.addEventListener('click', () => {
-    const item = btn.parentElement;
-    item.classList.toggle('open');
-  });
-});
--- examen/miligram/js/dist/acordeon.dev.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-
-document.querySelectorAll('.acordeon-titulo').forEach(function (btn) {
-  btn.addEventListener('click', function () {
-    var item = btn.parentElement;
-    item.classList.toggle('open');
-  });
-});
